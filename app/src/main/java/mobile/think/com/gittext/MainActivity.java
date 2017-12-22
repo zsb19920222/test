6aa5d7a435461b4ea5e3a7fa782b24c8f4528ca6
zhanshaobo <zhanshaobo1992@gmail.com>
2017-12-22 14:45:28 +0800

    Git2版本提交

--- app/src/main/java/mobile/think/com/gittext/MainActivity.java
+++ app/src/main/java/mobile/think/com/gittext/MainActivity.java
@@ -1,13 +1,17 @@
 package mobile.think.com.gittext;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn);
+        TextView tv = findViewById(R.id.tv);
     }
 }
